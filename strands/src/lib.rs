3b28c62a029cd0dc0bcb2fbdb7efd6537397ab26
Joshua Black <joshuablack.ecommerce@gmail.com>
2023-08-08 20:50:43 -0500

    Initial layout

---
+++ strands/src/lib.rs
@@ -0,0 +1,17 @@
+pub struct Packet;
+
+struct UdpData{
+    
+}
+
+struct UdpConnection{
+    data: UdpData,
+}
+
+struct ReliableExchange{
+    connection: UdpConnection,
+}
+
+struct UnreliableExchange{
+    
+}
